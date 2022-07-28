23fdf0c930388d55b9612b53fcf3eb441792a89d
parth1596 <parthranjan1596@gmail.com>
2022-07-28 19:35:37 +0530

    updated application

--- spring5webapp-bootstrap-v2/src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ spring5webapp-bootstrap-v2/src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.javaspring;
+package guru.springframework.spring5webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaspringApplicationTests {
+class Spring5webappApplicationTests {
 
 	@Test
 	void contextLoads() {
